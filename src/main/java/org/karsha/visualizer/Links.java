54497fc4c6a0500afab7462e0fa6f4fbfa71d86a
gihantharanga <tharangarpg@gmail.com>
2015-01-19 01:38:57 -0800

    links.java commented

--- src/main/java/org/karsha/visualizer/Links.java
+++ src/main/java/org/karsha/visualizer/Links.java
@@ -2,8 +2,18 @@ package org.karsha.visualizer;
 
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
 
+/*
+ * getter and setters
+ * This is POJO object which models the edge between two node
+ * source is source node index 
+ * target is target node index
+ * type is edge appeared time interval such "sustain","episodic","weak"
+ * YEAR indicates this edge appeared in year or available in relevant year
+ * status is which contains edge faked edge or true edge fake edges for incomplete triad edge
+ * inedge indicates this edge is incoming edge for incomplete triad nodes
+ * */
 public class Links{
-
+	
 	int source;
 	int target;
 	String type;
