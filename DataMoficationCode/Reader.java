ba9b1c5064ba6dbfcc996a4e911291f27d47cc0f
gihan tharanga <tharangarpg@gmail.com>
2015-02-09 22:19:09 -0800

    Update Reader.java

--- DataMoficationCode/Reader.java
+++ DataMoficationCode/Reader.java
@@ -1,17 +1,24 @@
 package org.lsflbas.Reader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Reader {
 	
-	public static void main(String[] args) {
-		final long sTime = System.currentTimeMillis();
-		 	String filepath="C://Users/Hunter/Desktop/R-d3/Node.csv";
-		 	String path="C://Users/Hunter/Desktop/R-d3/OriginalEdges/edges-2013.csv";
-		 	String writerpath="C://Users/Hunter/Desktop/R-d3/jprg/edgeset-2013.csv";
-		 		 	
+	public static void main(String[] args) throws IOException {
+		
+		 	String filepath="C://Users/lsf-admin/Desktop/R-Code/node.csv";
+		 	
+		 	String year="2013";
+		 	
+		 	String yearly="C://Users/lsf-admin/Desktop/R-Code/Yealy Granger Causality/edges/edge."+year+".csv";
+		 	String quartely="C://Users/lsf-admin/Desktop/R-Code/Quately Granger Causality/Qedges/Q."+year+".csv";
+		 	String writepath="C://Users/lsf-admin/Desktop/R-Code/jprg/V2/edgesNew."+year+".csv";
+		 			
 		 	Edge e=new Edge();
 		 	e.Nodes(filepath);
-		 	e.EdgeReader(path,writerpath);
-		 	final long eTime = System.currentTimeMillis();
-		 	System.out.println(eTime-sTime);
+		 	e.EdgeReader(yearly,quartely,writepath);
+		 	//System.out.println(eTime-sTime);
 	}	
 
 }
