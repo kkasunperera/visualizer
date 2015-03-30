b0c41b9e7ba0611197d3e660a869f862b1b48bd5
tharindu <madushanka.best@gmail.com>
2015-03-30 10:26:05 +0530

    db query updated for annual data retrivals

--- src/main/java/DBconnect/QueryDB.java
+++ src/main/java/DBconnect/QueryDB.java
@@ -35,4 +35,30 @@ public class QueryDB {
 
 		return jsonArray;
 	}
+	public JSONArray getFromDB_Links(String query, Connection connection) {
+
+		JSONArray jsonArray = new JSONArray();
+
+		try {
+			java.sql.Statement statment = connection.createStatement();
+			ResultSet resultSet = statment.executeQuery(query);
+
+			while (resultSet.next()) {
+				int total_rows = resultSet.getMetaData().getColumnCount();
+				JSONObject obj = new JSONObject();
+				for (int i = 0; i < total_rows; i++) {
+					obj.put(resultSet.getMetaData().getColumnLabel(i + 1)
+							.toLowerCase(), resultSet.getObject(i + 1));
+				}
+				jsonArray.put(obj);
+			}
+
+			// System.out.println("llll");
+			// System.out.println(resultSet.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return jsonArray;
+	}
 }
