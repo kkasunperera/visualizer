0c8d0964bc51347a8c17a40c7d0025c0751315ca
tharindu <madushanka.best@gmail.com>
2015-04-08 12:25:43 +0530

    updated extended analysis view

--- src/main/webapp/js/dataAnalysis.js
+++ src/main/webapp/js/dataAnalysis.js
@@ -504,10 +504,10 @@ function Hindex_histogram(pos,file,axistxt) {
 								return "translate(0," + i * 20 + ")";
 							});
 
-			legend.append("rect").attr("x", width - 18).attr("width", 18).attr(
+			legend.append("rect").attr("x", width - 48).attr("width", 18).attr(
 					"height", 18).style("fill", color);
 
-			legend.append("text").attr("x", width - 24).attr("y", 9)
+			legend.append("text").attr("x", width - 54).attr("y", 9)
 					.attr("dy", ".35em").style("text-anchor", "end").text(function(d) {
 						return d;
 					});
