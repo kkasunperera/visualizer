610af6d6eaa94fa44cccb1d6689bb230358896ea
Kasun Perera <kkasunperera@gmail.com>
2015-04-09 16:17:06 +0530

    modifying syscinfig file

--- src/main/java/system_config/R_sysConfig.java
+++ src/main/java/system_config/R_sysConfig.java
@@ -6,21 +6,25 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class R_sysConfig {
-	
-	public Properties get_prog() {
-		
-		Properties prog = new Properties();
-		try {
-			InputStream path = getClass().getResourceAsStream("sys.configurations");		
-			prog.load(path);
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return prog;
-	}
-	
-	
+
+    public Properties get_prog() {
+
+        Properties prog = new Properties();
+        try {
+            InputStream path = getClass().getClassLoader().getResourceAsStream("db.properties");
+
+            if (path != null) {
+                prog.load(path);
+            } else {
+                System.out.println("property file  not found in the classpath");
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prog;
+    }
+
 }
